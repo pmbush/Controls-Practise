eb8ed36af27ccd7d580fd33f11b07d952358088d
Nik Bush <pmnbush@gmail.com>
2021-12-08 05:48:54 +0300

    fix and implemented number for button, textfield  and scroll

--- Controls Practise/ViewController.swift
+++ Controls Practise/ViewController.swift
@@ -14,17 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonNew: UIButton!
     @IBOutlet var switches: [UISwitch]!
     @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var text: UITextField!
+    @IBOutlet weak var textField: UITextField!
+    
+    var number: UInt8 = 128 {
+        didSet{
+            UpdateUI()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UpdateUI()
+    }
+    
+    /// Updates all outlets to number
+    func UpdateUI() {
+        buttonNew.setTitle("\(number)", for: [])
+        
+        //to do switches to number
+        
+        slider.value = Float(number)
+        textField.text = "\(number)"
+        
     }
 
- 
-
     @IBAction func buttonNewPressed() {
-        print(#line, #function)
+        number = UInt8((Int(number) + 1) % 256)
     }
     
     @IBAction func switchToodled(_ sender: UISwitch) {
@@ -32,12 +47,14 @@ class ViewController: UIViewController {
     }
    
     @IBAction func sladerMoved() {
-        print(#line, #function)
+        number = UInt8(slider.value)
     }
 
-    @IBAction func textAdded() {
-        print(#line, #function)
+    
+    @IBAction func textFieldEdited() {
+        number = UInt8(textField.text ?? "") ?? 128
     }
+    
     
 }
 
