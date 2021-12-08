cb21f46a802324ccd944f07e2afabdfef511829a
Nik Bush <pmnbush@gmail.com>
2021-12-08 04:08:22 +0300

    Initial Commit

---
+++ Controls Practise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controls Practise
+//
+//  Created by Nik on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
